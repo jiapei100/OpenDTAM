10daa40f9dbfae05e8df97376b61c9b7bcbe9a54
jiapei100 <jp4work@gmail.com>
2016-10-02 21:19:09 -0700

    Initial commit after 1) having changed namespace cv::gpu to namespace cv::cuda 2) cudaSafeCall to CV_CUDEV_SAFE_CALL 3) having changed namespace cv::gpu to namespace cv::cuda

--- Cpp/testprog.cpp
+++ Cpp/testprog.cpp
@@ -33,9 +33,7 @@
 const static bool valgrind=0;
 
 //A test program to make the mapper run
-using namespace cv;
-using namespace cv::gpu;
-using namespace std;
+using namespace cv::cuda;
 
 int App_main( int argc, char** argv );
 
@@ -67,18 +65,18 @@ int App_main( int argc, char** argv )
 #endif
 
     char filename[500];
-    Mat image, cameraMatrix, R, T;
-    vector<Mat> images,Rs,ds,Ts,Rs0,Ts0,D0;
-    vector<float> key,qual,vis,occ;
+    cv::Mat image, cameraMatrix, R, T;
+    std::vector<cv::Mat> images,Rs,ds,Ts,Rs0,Ts0,D0;
+    std::vector<float> key,qual,vis,occ;
 
-    Mat ret;//a place to return downloaded images to
+    cv::Mat ret;//a place to return downloaded images to
     
-    ofstream file("outscale.csv");
+    std::ofstream file("outscale.csv");
     
     double reconstructionScale=5/5.;
     int inc=1;
     for(int i=0;i>0||inc>0;i+=inc){
-        Mat tmp,d,image;
+        cv::Mat tmp,d,image;
         int offset=0;
         if(inc>0){
             
@@ -91,9 +89,9 @@ int App_main( int argc, char** argv )
                    cameraMatrix,
                    R,
                    T);
-        tmp=Mat::zeros(image.rows,image.cols,CV_32FC3);
+        tmp=cv::Mat::zeros(image.rows,image.cols,CV_32FC3);
         randu(tmp,0,1);
-        resize(image+tmp/255,image,Size(),reconstructionScale,reconstructionScale);
+        cv::cuda::resize(image+tmp/255,image,cv::Size(),reconstructionScale,reconstructionScale);
         images.push_back(image.clone());
         Rs.push_back(R.clone());
         Ts.push_back(T.clone());
@@ -119,35 +117,36 @@ int App_main( int argc, char** argv )
         qual.push_back(0);
         vis.push_back(0);
         occ.push_back(0);
-//         Rs.push_back(Mat());
-//         Ts.push_back(Mat());
+//         Rs.push_back(cv::Mat());
+//         Ts.push_back(cv::Mat());
         
         if(i==numImg-1)
             inc=-1;
     }
     numImg=numImg*2-2;
 //     {//random first image
-//         cout<<LieSub(RTToLie(Rs0[0],Ts0[0]),RTToLie(Rs0[1],Ts0[1]))<<endl;
+//         std::cout<<LieSub(RTToLie(Rs0[0],Ts0[0]),RTToLie(Rs0[1],Ts0[1]))<<std::endl;
 //         Ts[0]=Ts0[1].clone();
 //         Ts[1]=Ts0[1].clone();
 //         randu(Ts[1] ,Scalar(-1),Scalar(1));
 //         Ts[1]=Ts0[0]+Ts[1];
-//         cout<<Ts[1]-Ts0[0]<<endl;
+//         std::cout<<Ts[1]-Ts0[0]<<std::endl;
 //         Rs[0]=Rs0[0].clone();
 //         Rs[1]=Rs0[0].clone();
 //     }
-    CudaMem cret(images[0].rows,images[0].cols,CV_32FC1);
-    ret=cret.createMatHeader();
+//    CudaMem cret(images[0].rows,images[0].cols,CV_32FC1);
+//    ret=cret.createMatHeader();
+    cv::Mat cret(images[0].rows,images[0].cols,CV_32FC1);
     //Setup camera matrix
     double sx=reconstructionScale;
     double sy=reconstructionScale;
-    cameraMatrix+=(Mat)(Mat_<double>(3,3) <<    0.0,0.0,0.5,
+    cameraMatrix+=(cv::Mat)(cv::Mat_<double>(3,3) <<    0.0,0.0,0.5,
                                                 0.0,0.0,0.5,
                                                 0.0,0.0,0.0);
-    cameraMatrix=cameraMatrix.mul((Mat)(Mat_<double>(3,3) <<    sx,0.0,sx,
+    cameraMatrix=cameraMatrix.mul((cv::Mat)(cv::Mat_<double>(3,3) <<    sx,0.0,sx,
                                                                 0.0,sy ,sy,
                                                                 0.0,0.0,1.0));
-    cameraMatrix-=(Mat)(Mat_<double>(3,3) <<    0.0,0.0,0.5,
+    cameraMatrix-=(cv::Mat)(cv::Mat_<double>(3,3) <<    0.0,0.0,0.5,
                                                 0.0,0.0,0.5,
                                                 0.0,0.0,0);
     int layers=128;
@@ -167,18 +166,18 @@ int App_main( int argc, char** argv )
     
  
     
-    cv::gpu::Stream s;
+    cv::cuda::Stream s;
     double totalscale=1.0;
     int tcount=0;
     int sincefail=0;
     for (int imageNum=(startAt+1)%numImg;imageNum<numImg;imageNum=(imageNum+1)%numImg){
-        cout<<dec;
+ //       std::cout<<dec; // deleted by Pei JIA
         T=Ts[imageNum].clone();
         R=Rs[imageNum].clone();
         image=images[imageNum];
 
         if(cv.count<imagesPerCV){
-            cout<<"using: "<< imageNum<<endl;
+            std::cout<<"using: "<< imageNum<<std::endl;
             cv.updateCost(image, R, T);
             cudaDeviceSynchronize();
 //             gpause();
@@ -189,7 +188,7 @@ int App_main( int argc, char** argv )
 
 //             volatile int keep=1;
 // //             while(cv.count==190&&keep){
-//                 vector<Mat> slices=cv.download();
+//                 std::vector<cv::Mat> slices=cv.download();
 //                 for( int i=0;i<cv.rows;i++){
 //                     pfShow("cross Section",slices[i],0, cv::Vec2d(0, 1));
 // //                     usleep(10000);
@@ -202,20 +201,20 @@ int App_main( int argc, char** argv )
             
             for(int i0=1;i0<=10-imagesPerCV;i0++){
                 int i=((cv.fid-i0)%numImg+numImg)%numImg;
-                cout<<"using: "<< i<<endl;
+                std::cout<<"using: "<< i<<std::endl;
                 cv.updateCost(images[i], Rs[i], Ts[i]);
                 cudaDeviceSynchronize();
             }
             
             cudaDeviceSynchronize();
             //Attach optimizer
-            Ptr<DepthmapDenoiseWeightedHuber> dp = createDepthmapDenoiseWeightedHuber(cv.baseImageGray,cv.cvStream);
+            cv::Ptr<DepthmapDenoiseWeightedHuber> dp = createDepthmapDenoiseWeightedHuber(cv.baseImageGray,cv.cvStream);
             DepthmapDenoiseWeightedHuber& denoiser=*dp;
             Optimizer optimizer(cv);
             optimizer.initOptimization();
             GpuMat a(cv.loInd.size(),cv.loInd.type());
-//             cv.loInd.copyTo(a,cv.cvStream);
-            cv.cvStream.enqueueCopy(cv.loInd,a);
+             cv.loInd.copyTo(a,cv.cvStream);
+//            cv.cvStream.enqueueCopy(cv.loInd,a);
             GpuMat d;
             denoiser.cacheGValues();
             ret=image*0;
@@ -233,7 +232,7 @@ int App_main( int argc, char** argv )
 
             bool doneOptimizing; int Acount=0; int QDcount=0;
             do{
-//                 cout<<"Theta: "<< optimizer.getTheta()<<endl;
+//                 std::cout<<"Theta: "<< optimizer.getTheta()<<std::endl;
 //
 //                 if(Acount==0)
 //                     gpause();
@@ -271,37 +270,37 @@ int App_main( int argc, char** argv )
             pfShow("loVal", ret, 0, cv::Vec2d(0, 3));
              cv.loInd.download(ret);
             pfShow("loInd", ret, 0, cv::Vec2d(0, layers));
-            cout<<"diffN0:"<<sum(abs(D0[cv.fid]-(ret*cv.depthStep+cv.far)))<<endl;
-            cout<<"diffN0Med:"<<median(abs(D0[cv.fid]-(ret*cv.depthStep+cv.far)))<<endl;
+            std::cout<<"diffN0:"<<cv::cuda::sum(abs(D0[cv.fid]-(ret*cv.depthStep+cv.far)))<<std::endl;
+            std::cout<<"diffN0Med:"<<median(abs(D0[cv.fid]-(ret*cv.depthStep+cv.far)))<<std::endl;
             medianBlur(ret,ret,3);
             medianBlur(ret,ret,3);
             pfShow("loIndMed",ret, 0, cv::Vec2d(0, layers));
-            cout<<"diffMedN0:"<<sum(abs(D0[cv.fid]-(ret*cv.depthStep+cv.far)))<<endl;
-            cout<<"diffMedN0Med:"<<median(abs(D0[cv.fid]-(ret*cv.depthStep+cv.far)))<<endl;
+            std::cout<<"diffMedN0:"<<cv::cuda::sum(abs(D0[cv.fid]-(ret*cv.depthStep+cv.far)))<<std::endl;
+            std::cout<<"diffMedN0Med:"<<median(abs(D0[cv.fid]-(ret*cv.depthStep+cv.far)))<<std::endl;
             a.download(ret);
             pfShow("A function loose", ret, 0, cv::Vec2d(0, layers));
-            Mat diff=ret.clone();
+            cv::Mat diff=ret.clone();
             cv.loInd.download(ret);
             diff-=ret;
             pfShow("difference by reg", diff, 0, cv::Vec2d(-layers, layers));
             pfShow("true Depth", D0[cv.fid], 0, cv::Vec2d(0, cv.near));
             pfShow("diff", D0[cv.fid]-optimizer.depthMap(), 0, cv::Vec2d(-.005,.005));
-            cout<<"diff:"<<sum(abs(D0[cv.fid]-optimizer.depthMap()))<<endl;
-            cout<<"diffMed:"<<median(abs(D0[cv.fid]-optimizer.depthMap()))<<endl;
+            std::cout<<"diff:"<<cv::cuda::sum(abs(D0[cv.fid]-optimizer.depthMap()))<<std::endl;
+            std::cout<<"diffMed:"<<median(abs(D0[cv.fid]-optimizer.depthMap()))<<std::endl;
             
 //                gpause();
-//             cout<<"A iterations: "<< Acount<< "  QD iterations: "<<QDcount<<endl;
+//             std::cout<<"A iterations: "<< Acount<< "  QD iterations: "<<QDcount<<std::endl;
 //             pfShow("Depth Solution", optimizer.depthMap(), 0, cv::Vec2d(cv.far, cv.near));
 //             imwrite("outz.png",ret);
 
 //             Track tracker(cv);
-//             Mat out=optimizer.depthMap();
+//             cv::Mat out=optimizer.depthMap();
 //             
 // //             sprintf(filename,"/groundtruth/depth_%03d.png",cv.fid);
-// //             Mat out16;
+// //             cv::Mat out16;
 // //             out16=1/out;
 // //             out16.convertTo(out16,CV_16UC3,10);
-// //             cout<<"Mean:"<<mean(out16)[0]<<endl;
+// //             std::cout<<"Mean:"<<mean(out16)[0]<<std::endl;
 // //             imwrite(filename,out16);
 //             
 // //             if (tcount==3){
@@ -314,8 +313,8 @@ int App_main( int argc, char** argv )
 //                 
             double sf=1;//(.25*cv.near/m);
 //             if(!(sf<100&&sf>.001)){
-// //                 file<<sf<<", fail!, "<<endl;
-//                 cout<<"FAIL CV #: "<<tcount<<" sf: "<<sf<<endl;
+// //                 file<<sf<<", fail!, "<<std::endl;
+//                 std::cout<<"FAIL CV #: "<<tcount<<" sf: "<<sf<<std::endl;
 //                 if(sf>100||sf<.001)
 //                     sf=1.0+.1-.2*(sf<1.0);
 // //                 gpause();
@@ -328,7 +327,7 @@ int App_main( int argc, char** argv )
             int ni=0;
 //             ni=imagesPerCV;
 //             ni=min(ni,imagesPerCV-3);
-            ni=max(ni,1);
+            ni=cv::max(ni,1);
             if(tcount==0)
                 ni=1;
             imageNum=((imageNum-imagesPerCV-1+ni)%numImg+numImg)%numImg;
@@ -354,7 +353,7 @@ int App_main( int argc, char** argv )
 //                 if(!tracker.align()){
 //                     int neg=((i-1)%numImg+numImg)%numImg;
 //                     if(i0<2 && ((i-cv.fid)%numImg+numImg)%numImg==1){//failed to align the next image and at limit on frames
-//                         cout<<"FAil: "<<i<<" on: "<<cv.fid<<endl;
+//                         std::cout<<"FAil: "<<i<<" on: "<<cv.fid<<std::endl;
 // //                         tracker.verbose=1;
 // //                         tracker.pose=RTToLie(Rs[neg],Ts[neg]);
 // //                         tracker.thisFrame=images[neg];
@@ -365,7 +364,7 @@ int App_main( int argc, char** argv )
 //                     }
 //                     imagesPerCV=max(i0-1,1);
 // //                     if(i0==0&&sincefail>4){
-// //                         cout<<"TRACKFAIL! RESTART RANDOM"<<endl;
+// //                         std::cout<<"TRACKFAIL! RESTART RANDOM"<<std::endl;
 // //                         sf=cv.near/.15;//failed so bad we need a new start
 // // //                         randu(tracker.depth ,Scalar(0),Scalar(.15));
 // //                         tracker.depth=.10;
@@ -392,32 +391,32 @@ int App_main( int argc, char** argv )
 //                 }
 // 
 //                 skip:
-//                 Mat p,tp;
+//                 cv::Mat p,tp;
 //                 p=tracker.pose;
 //                 tp=RTToLie(Rs0[i],Ts0[i]);
 // // //                 {//debug
-// // //                     cout << "True Pose: "<< tp << endl;
-// // //                     cout << "True Delta: "<< LieSub(tp,tracker.basePose) << endl;
-//                     cout << "Recovered Pose: "<< p << endl;
-// // //                     cout << "Recovered Delta: "<< LieSub(p,tracker.basePose) << endl;
-// // //                     cout << "Pose Error: "<< p-tp << endl;
+// // //                     std::cout << "True Pose: "<< tp << std::endl;
+// // //                     std::cout << "True Delta: "<< LieSub(tp,tracker.basePose) << std::endl;
+//                     std::cout << "Recovered Pose: "<< p << std::endl;
+// // //                     std::cout << "Recovered Delta: "<< LieSub(p,tracker.basePose) << std::endl;
+// // //                     std::cout << "Pose Error: "<< p-tp << std::endl;
 // // //                 }
 //                 
-//                 cout<<"<"<<cv.fid<<", "<<i<<">"<<endl;
-// //                 Mat tran1=Mat::eye(4,4,CV_64FC1);
-// //                 ((Mat)(Mat_<double>(4,1) <<    0,0,-1.0/m,1)).copyTo(tran1.col(3));
-// //                 Mat rotor=make4x4(rodrigues((Mat)(Mat_<double>(3,1) << 0,-45,0)*3.1415/180.0));
-// //                 Mat tran2=Mat::eye(4,4,CV_64FC1);
-// //                 ((Mat)(Mat_<double>(4,1) <<    0,0,3/m,1)).copyTo(tran2.col(3));
-// //                 Mat view=tran2*rotor*tran1;
-//                 Mat basePose=make4x4(RTToP(Rs[cv.fid],Ts[cv.fid]));
-//                 Mat basePose0=make4x4(RTToP(Rs0[cv.fid],Ts0[cv.fid]));
-//                 Mat foundPose=make4x4(RTToP(R,T));
-// // //                 cout<<"view:\n"<< fixed << setprecision(3)<< view<<endl;
-//                 Mat view=diagnosticInfo(images[i],images[cv.fid],tracker.depth,basePose,foundPose,cameraMatrix);
-// //                 Mat viewc=diagnosticInfo(images[i],images[cv.fid],tracker.depth,basePose0,make4x4(RTToP(Rs0[i],Ts0[i])),cameraMatrix);
+//                 std::cout<<"<"<<cv.fid<<", "<<i<<">"<<std::endl;
+// //                 cv::Mat tran1=cv::Mat::eye(4,4,CV_64FC1);
+// //                 ((cv::Mat)(cv::Mat_<double>(4,1) <<    0,0,-1.0/m,1)).copyTo(tran1.col(3));
+// //                 cv::Mat rotor=make4x4(rodrigues((cv::Mat)(cv::Mat_<double>(3,1) << 0,-45,0)*3.1415/180.0));
+// //                 cv::Mat tran2=cv::Mat::eye(4,4,CV_64FC1);
+// //                 ((cv::Mat)(cv::Mat_<double>(4,1) <<    0,0,3/m,1)).copyTo(tran2.col(3));
+// //                 cv::Mat view=tran2*rotor*tran1;
+//                 cv::Mat basePose=make4x4(RTToP(Rs[cv.fid],Ts[cv.fid]));
+//                 cv::Mat basePose0=make4x4(RTToP(Rs0[cv.fid],Ts0[cv.fid]));
+//                 cv::Mat foundPose=make4x4(RTToP(R,T));
+// // //                 std::cout<<"view:\n"<< fixed << setprecision(3)<< view<<std::endl;
+//                 cv::Mat view=diagnosticInfo(images[i],images[cv.fid],tracker.depth,basePose,foundPose,cameraMatrix);
+// //                 cv::Mat viewc=diagnosticInfo(images[i],images[cv.fid],tracker.depth,basePose0,make4x4(RTToP(Rs0[i],Ts0[i])),cameraMatrix);
 // //                 for(int j=0;j<5;j++){
-// //                     Mat tmp;
+// //                     cv::Mat tmp;
 // //                     pfShow("Predicted Image",view,0,Vec2d(0,1));
 // //                     absdiff(images[i],view,tmp);
 // //                     pfShow("difftrk",tmp,0,Vec2d(0,1));
@@ -442,16 +441,16 @@ int App_main( int argc, char** argv )
             
 //             for (int a=0;a<360;a+=1)
 //             {
-//             Mat tran1=Mat::eye(4,4,CV_64FC1);
-//             ((Mat)(Mat_<double>(4,1) <<    0,-100,-400,1)).copyTo(tran1.col(3));
-//             Mat rotor=make4x4(rodrigues((Mat)(Mat_<double>(3,1) << 0,a,0)*3.1415926535/180.0)*
-//                 rodrigues((Mat)(Mat_<double>(3,1) << 90,0,0)*3.1415926535/180.0)
+//             cv::Mat tran1=cv::Mat::eye(4,4,CV_64FC1);
+//             ((cv::Mat)(cv::Mat_<double>(4,1) <<    0,-100,-400,1)).copyTo(tran1.col(3));
+//             cv::Mat rotor=make4x4(rodrigues((cv::Mat)(cv::Mat_<double>(3,1) << 0,a,0)*3.1415926535/180.0)*
+//                 rodrigues((cv::Mat)(cv::Mat_<double>(3,1) << 90,0,0)*3.1415926535/180.0)
 //             );
-//             Mat tran2=Mat::eye(4,4,CV_64FC1);
-//             ((Mat)(Mat_<double>(4,1) <<    0,0,3/m,1)).copyTo(tran2.col(3));
-//             Mat view=tran2*rotor*tran1;
-//             Mat basePose=make4x4(RTToP(Rs[cv.fid],Ts[cv.fid]));
-//             Mat foundPose=make4x4(RTToP(Rs[imageNum],Ts[imageNum]));
+//             cv::Mat tran2=cv::Mat::eye(4,4,CV_64FC1);
+//             ((cv::Mat)(cv::Mat_<double>(4,1) <<    0,0,3/m,1)).copyTo(tran2.col(3));
+//             cv::Mat view=tran2*rotor*tran1;
+//             cv::Mat basePose=make4x4(RTToP(Rs[cv.fid],Ts[cv.fid]));
+//             cv::Mat foundPose=make4x4(RTToP(Rs[imageNum],Ts[imageNum]));
 //             diagnosticInfo(images[imageNum],images[cv.fid],tracker.depth,basePose,view,cameraMatrix);
 //             }
 
@@ -464,13 +463,13 @@ int App_main( int argc, char** argv )
             
             
             totalscale*=sf;
-            file<<imageNum<<", "<<sf<<", "<<imagesPerCV<<endl;
+            file<<imageNum<<", "<<sf<<", "<<imagesPerCV<<std::endl;
 //             file.sync_with_stdio();
             if(tcount==7){
                 totalscale=1.0f;
             }
             tcount++;
-            cout<<"CV #: "<<tcount<<" Total Scale: "<<totalscale<<endl;
+            std::cout<<"CV #: "<<tcount<<" Total Scale: "<<totalscale<<std::endl;
             s=optimizer.cvStream;
 //             for (int imageNum=0;imageNum<numImg;imageNum=imageNum+1){
 //                 reprojectCloud(images[imageNum],images[0],optimizer.depthMap(),RTToP(Rs[0],Ts[0]),RTToP(Rs[imageNum],Ts[imageNum]),cameraMatrix);
@@ -481,13 +480,13 @@ int App_main( int argc, char** argv )
         s.waitForCompletion();// so we don't lock the whole system up forever
     }
     exit:
-    file<<"Key_is_keyframes"<<endl;
-    file<<"Occlusion_is_frac_lost_by_occlusion"<<endl;
-    file<<"Coverage_is_frac_not_lost_by_occlusion_or_out_of_frame"<<endl;
-    file<<"Quality_is_frac_of_covered_that_matches"<<endl;
-    file<<"Key,Quality,Coverage,Occlusion"<<endl;
+    file<<"Key_is_keyframes"<<std::endl;
+    file<<"Occlusion_is_frac_lost_by_occlusion"<<std::endl;
+    file<<"Coverage_is_frac_not_lost_by_occlusion_or_out_of_frame"<<std::endl;
+    file<<"Quality_is_frac_of_covered_that_matches"<<std::endl;
+    file<<"Key,Quality,Coverage,Occlusion"<<std::endl;
     for(int i=0;i<numImg;i++)
-        file<<key[i]<<","<<qual[i]<<","<<vis[i]<<","<<occ[i]<<endl;
+        file<<key[i]<<","<<qual[i]<<","<<vis[i]<<","<<occ[i]<<std::endl;
     s.waitForCompletion();
     Stream::Null().waitForCompletion();
     return 0;
